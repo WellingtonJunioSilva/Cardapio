ac91b2a01310161255a132ecb7bf6ae5406464c1
Wellington Juno <wjuniossantos009@gmail.com>
2025-06-17 08:13:39 -0300

    Enviando commit para a main

---
+++ Cardapio/src/main/java/com/example/Cardapio/Controller/FoodController.java
@@ -0,0 +1,32 @@
+package com.example.Cardapio.Controller;
+
+import com.example.Cardapio.DTO.FoodRequestDTO;
+import com.example.Cardapio.DTO.FoodResponseDTO;
+import com.example.Cardapio.Entitys.Food;
+import com.example.Cardapio.Repositories.FoodRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("food")
+public class FoodController {
+    @Autowired
+    private FoodRepository repository;
+
+    @PostMapping
+    public void saveFood(@RequestBody FoodRequestDTO data){
+        Food foodData = new Food(data);
+        repository.save(foodData);
+        return;
+
+    }
+
+    @GetMapping
+    public List<FoodResponseDTO> getAll(){
+        List<FoodResponseDTO> foodList = repository.findAll().stream().map(FoodResponseDTO::new).toList();
+        return foodList;
+
+    }
+}
---
+++ Cardapio/src/main/java/com/example/Cardapio/DTO/FoodRequestDTO.java
@@ -0,0 +1,5 @@
+package com.example.Cardapio.DTO;
+
+public record FoodRequestDTO(String title, String image, String Description, Integer price) {
+
+}
